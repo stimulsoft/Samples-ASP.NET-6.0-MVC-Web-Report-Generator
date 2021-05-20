a280880bc117be06721f9e92eb77cb07c96ce9c7
VladimirHot <hotchenko@gmail.com>
2021-05-20 18:07:43 +0300

    new release v2021.3.1

---
+++ Change Viewer Options from the Controller/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Change_Viewer_Options_from_the_Controller.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
