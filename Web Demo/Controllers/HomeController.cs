ad49569fb7ffc8d256c149a0683e627ec3aab457
Tim Kubekin <68532402+tsimafeikubekin@users.noreply.github.com>
2022-01-29 11:51:53 +0300

    new design for web demo project

--- Web Demo/Controllers/HomeController.cs
+++ Web Demo/Controllers/HomeController.cs
@@ -22,16 +22,5 @@ namespace Web_Demo.Controllers
         {
             return View();
         }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
